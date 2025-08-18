5dc2ff31093b0769f343bbcf8d8473a4eecf044c
Muhammad Rizki Kurnia Putra <muhammadrizki05@gmail.com>
2025-08-18 09:40:34 +0700

    add nav and header

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,5 @@
 import type { Route } from "./+types/home";
+import { Link } from "react-router-dom";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -21,8 +22,22 @@ export default function Home({ loaderData }: Route.ComponentProps) {
   const products = loaderData;
 
   return (
-    <div>
-      <h1>R Sport Store</h1>
+    <div className="min-h-screen flex flex-col bg-[#7f3b3b] text-black">
+      {/* Header */}
+      <header className="flex justify-between items-center p-4">
+        <div>
+          <h1 className="text-2xl font-bold">GLORY STORE</h1>
+        </div>
+        <nav className="flex gap-4">
+          <Link to="/" className="hover:underline">
+            Home
+          </Link>
+          <Link to="/login" className="hover:underline">
+            Login
+          </Link>
+        </nav>
+      </header>
+
       <ul>
         {products.map((product: any) => {
           return <li key={product.id}>{product.name}</li>;
