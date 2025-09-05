2af9a4b557725ac4e4628d053a1d580db4639242
Muhammad Rizki Kurnia Putra <muhammadrizki05@gmail.com>
2025-09-06 02:22:19 +0700

    feat: layout sticky footer

--- app/layouts/layout-main.tsx
+++ app/layouts/layout-main.tsx
@@ -5,10 +5,13 @@ import { Header } from "../components/header";
 
 export default function LayoutMain() {
   return (
-    <div>
+    <div className="flex min-h-screen flex-col">
       <Header />
       {/* will either be home.tsx or settings.tsx */}
-      <Outlet />
+      <main className="flex-[1]">
+        <Outlet />
+      </main>
+
       <Footer />
     </div>
   );
