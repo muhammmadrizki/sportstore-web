a5fa194e9446679605ddd751a9f055a37fc458ed
Muhammad Rizki Kurnia Putra <muhammadrizki05@gmail.com>
2025-09-07 14:12:39 +0700

    feat: dashboard and user.ts

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,25 +1,40 @@
+import { Card } from "~/components/ui/card";
 import { getSession } from "../sessions";
 import type { Route } from "./+types/dashboard";
+import type { UserAuthMe } from "~/modules/user/type";
+import { redirect } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "Dashboard" }];
 }
 
-export async function clientLoader({ request }: Route.ClientLoaderArgs) {
+export async function loader({ request }: Route.ClientLoaderArgs) {
   const session = await getSession(request.headers.get("Cookie"));
-
-  // console.log(session);
+  if (!session.has("token")) {
+    return redirect("/login");
+  }
 
   const token = session.get("token");
-  console.log({ token });
-
-  return null;
+  const response = await fetch(
+    `${import.meta.env.VITE_BACKEND_API_URL}/auth/me`,
+    { headers: { Authorization: `Bearer ${token}` } }
+  );
+  const user: UserAuthMe = await response.json();
+  return { user };
 }
 
-export default function DashboardRoute({}: Route.ComponentProps) {
+export default function DashboardRoute({ loaderData }: Route.ComponentProps) {
+  const { user } = loaderData;
+
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="flex flex-col items-center">
+      <div className="w-full max-w-xs">
+        <h1>Dashboard</h1>
+        <Card>
+          <h2>{user.fullName}</h2>
+          <p>{user.email}</p>
+        </Card>
+      </div>
     </div>
   );
 }
