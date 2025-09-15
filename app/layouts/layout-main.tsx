c56fcf2d5723f03ac3d77f9a6ddd395931fb4452
Muhammad Rizki Kurnia Putra <muhammadrizki05@gmail.com>
2025-09-15 21:28:24 +0700

    fix: layout main

--- app/layouts/layout-main.tsx
+++ app/layouts/layout-main.tsx
@@ -1,47 +1,76 @@
-import { Outlet } from "react-router";
-import { Footer } from "../components/footer";
-import { Header } from "../components/header";
-import type { UserAuthMe } from "~/modules/user/type";
-import { getSession } from "../sessions";
+import { Link, Outlet } from "react-router";
 import type { Route } from "./+types/layout-main";
+import { getSession } from "../sessions";
+import type { UserAuthMe } from "~/modules/user/type";
 
-// const navigationLinks = [
-//   { to: "/", text: "Home" },
-//   { to: "/products", text: "Products" },
-//   { to: "/register", text: "Register" },
-//   { to: "/login", text: "Login" },
-//   { to: "/dashboard", text: "Dashboard" },
-// ];
-const navigationLinks = [
+const navigationLinksDefault = [
   { to: "/", text: "Home" },
   { to: "/products", text: "Products" },
+  { to: "/register", text: "Register" },
+  { to: "/login", text: "Login" },
+  { to: "/dashboard", text: "Dashboard" },
+];
+
+const navigationLinksAuth = [
+  { to: "/", text: "Home" },
+  { to: "/products", text: "Products" },
+  { to: "/logout", text: "Logout" },
   { to: "/dashboard", text: "Dashboard" },
 ];
 
 export async function loader({ request }: Route.ClientLoaderArgs) {
   const session = await getSession(request.headers.get("Cookie"));
   const token = session.get("token");
+
   const response = await fetch(
     `${import.meta.env.VITE_BACKEND_API_URL}/auth/me`,
     { headers: { Authorization: `Bearer ${token}` } }
   );
   const user: UserAuthMe = await response.json();
-  return { user };
+  const isAuthenticated = token ? true : false;
+  const navigationLinks = isAuthenticated
+    ? navigationLinksAuth
+    : navigationLinksDefault;
+  return { user, navigationLinks };
 }
 
 export default function LayoutMain({ loaderData }: Route.ComponentProps) {
-  const { user } = loaderData;
+  const { user, navigationLinks } = loaderData;
 
   return (
-    <div className="flex min-h-screen flex-col">
-      <Header />
-      {/* <Header navigationLinks={navigationLinks} user={user} /> */}
-      {/* will either be home.tsx or settings.tsx */}
+    <>
+      <header className="flex justify-between items-center p-4 bg-[#7f3b3b] text-white">
+        <Link to="/" className="hover:underline font-bold text-2xl">
+          GLORY STORE
+        </Link>
+
+        {/* Navigation Links */}
+        <nav className="flex space-x-6 text-sm md:text-base">
+          {navigationLinks.map((link) => (
+            <Link
+              key={link.to}
+              to={link.to}
+              className="hover:underline transition-colors duration-200"
+            >
+              {link.text}
+            </Link>
+          ))}
+          {user.fullName && (
+            <span className="ml-4 text-black">{user.fullName}</span>
+          )}
+        </nav>
+      </header>
       <main className="flex-[1]">
         <Outlet />
       </main>
-
-      <Footer />
-    </div>
+      <footer>
+        <div className="flex justify-center m-5">
+          <p>
+            &copy; {new Date().getFullYear()} Created by. Muhammad Rizki Kurnia
+            Putra
+          </p>
+        </div>
+      </footer>
+    </>
   );
 }
